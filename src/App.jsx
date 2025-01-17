feec3ea12137688ca3365bcb0067d587df0259cd
Gradi <tshiabutagradi@gmail.com>
2025-01-17 19:46:33 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 // import Navbar from './components/Navbar';
-import WaterAnimationHero from './components/WaterAnimationHero';
+import FlameAnimationHero from './components/FlameAnimationHero';
 // import InteractiveCard from './components/InteractiveCard';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <WaterAnimationHero />      
+      <FlameAnimationHero />      
     </div>
   );
 }
